d5489c5a81a73f6b95bb9a1ac2f163cb32651fa4
kentvillamora-dev <kentvillamora.dev@gmail.com>
2026-08-31 12:15:03 +0800

    Added full default product list

--- src/features/inventory/defaultProducts.ts
+++ src/features/inventory/defaultProducts.ts
@@ -2,62 +2,828 @@ import type { AnyProduct } from './productTypes'
 
 export const defaultProducts: AnyProduct[] = [
   {
-    name: 'Coke 1L',
-    category: 'Beverages',
-    subcategory: 'Soft Drinks'
+    name: 'Choc Nut',
+    category: 'Snacks',
+    subcategory: 'Candies'
   },
-
   {
-    name: 'Nescafe 3n1 Original',
-    category: 'Coffee & Milk',
-    subcategory: 'Coffee'
+    name: 'Flat Tops',
+    category: 'Snacks',
+    subcategory: 'Candies'
   },
-
+  {
+    name: 'Judge',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'Maxx Honey Lemon',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'Maxx Menthol',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'Mentos Fruit',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'Mentos Mint',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'Potchi',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'Snow Bear',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'White Rabbit',
+    category: 'Snacks',
+    subcategory: 'Candies'
+  },
+  {
+    name: 'Bingo Chocolate',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Bingo Orange',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Cream-O',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Fita',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Hansel Milk',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Hansel Mocha',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
   {
     name: 'Magic Flakes',
     category: 'Snacks',
     subcategory: 'Biscuits'
   },
-
   {
-    name: 'Lucky Me Pancit Canton',
+    name: 'Presto Peanut Butter',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Presto Chocolate',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Rebisco Crackers',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Rebisco Sandwich Chocolate',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Rebisco Sandwich Strawberry',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'SkyFlakes',
+    category: 'Snacks',
+    subcategory: 'Biscuits'
+  },
+  {
+    name: 'Boy Bawang',
+    category: 'Snacks',
+    subcategory: 'Small Chips'
+  },
+  {
+    name: 'Ding Dong',
+    category: 'Snacks',
+    subcategory: 'Small Chips'
+  },
+  {
+    name: 'Super Crunch',
+    category: 'Snacks',
+    subcategory: 'Small Chips'
+  },
+  {
+    name: 'Cheezy Corn Crunch',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Chippy BBQ',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Clover Chips Cheese',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Loaded',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Martys Cracklin',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Mr. Chips',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Nova Country Cheddar',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Nova Homestyle BBQ',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Oishi Cracklings',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Oishi Pillows Chocolate',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Oishi Potato Fries',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Oishi Prawn Crackers',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Piattos Cheese',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Piattos Sour Cream',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Roller Coaster',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'V-Cut Spicy BBQ',
+    category: 'Snacks',
+    subcategory: 'Big Chips'
+  },
+  {
+    name: 'Fudgee Barr Chocolate',
+    category: 'Snacks',
+    subcategory: 'Minicakes'
+  },
+  {
+    name: 'Fudgee Barr Vanilla',
+    category: 'Snacks',
+    subcategory: 'Minicakes'
+  },
+  {
+    name: 'Choco Mucho',
+    category: 'Snacks',
+    subcategory: 'Chocolates'
+  },
+  {
+    name: 'Cloud 9',
+    category: 'Snacks',
+    subcategory: 'Chocolates'
+  },
+  {
+    name: 'Coke 1.5L',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Coke Litro',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Coke Mismo',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Mountain Dew',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Pepsi',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Royal 1.5L',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Royal Litro',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Royal Mismo',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Sprite 1.5L',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Sprite Litro',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Sprite Mismo',
+    category: 'Beverages',
+    subcategory: 'Soft Drinks'
+  },
+  {
+    name: 'Cobra',
+    category: 'Beverages',
+    subcategory: 'Energy Drinks'
+  },
+  {
+    name: 'Gatorade',
+    category: 'Beverages',
+    subcategory: 'Energy Drinks'
+  },
+  {
+    name: 'Sting',
+    category: 'Beverages',
+    subcategory: 'Energy Drinks'
+  },
+  {
+    name: 'Nature’s Spring',
+    category: 'Beverages',
+    subcategory: 'Water'
+  },
+  {
+    name: 'Wilkins',
+    category: 'Beverages',
+    subcategory: 'Water'
+  },
+  {
+    name: 'C2',
+    category: 'Beverages',
+    subcategory: 'Other Beverages'
+  },
+  {
+    name: 'Chuckie',
+    category: 'Beverages',
+    subcategory: 'Other Beverages'
+  },
+  {
+    name: 'Zesto',
+    category: 'Beverages',
+    subcategory: 'Other Beverages'
+  },
+  {
+    name: 'Great Taste Choco',
+    category: 'Coffee & Milk',
+    subcategory: 'Coffee'
+  },
+  {
+    name: 'Great Taste White',
+    category: 'Coffee & Milk',
+    subcategory: 'Coffee'
+  },
+  {
+    name: 'Kopiko Blanca',
+    category: 'Coffee & Milk',
+    subcategory: 'Coffee'
+  },
+  {
+    name: 'Kopiko Brown',
+    category: 'Coffee & Milk',
+    subcategory: 'Coffee'
+  },
+  {
+    name: 'Nescafe 3n1 Original',
+    category: 'Coffee & Milk',
+    subcategory: 'Coffee'
+  },
+  {
+    name: 'Nescafe Stick',
+    category: 'Coffee & Milk',
+    subcategory: 'Coffee'
+  },
+  {
+    name: 'Nescafe 3n1 Creamy White',
+    category: 'Coffee & Milk',
+    subcategory: 'Coffee'
+  },
+  {
+    name: 'Alaska Condensada',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Alaska Evaporated',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Angel Condensada',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Angel Evaporada',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Bear Brand',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Birch Tree',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Energen Chocolate',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Energen Vanilla',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Milo',
+    category: 'Coffee & Milk',
+    subcategory: 'Milk'
+  },
+  {
+    name: 'Eight O’Clock',
+    category: 'Coffee & Milk',
+    subcategory: 'Juice'
+  },
+  {
+    name: 'Tang Mango',
+    category: 'Coffee & Milk',
+    subcategory: 'Juice'
+  },
+  {
+    name: 'Tang Orange',
+    category: 'Coffee & Milk',
+    subcategory: 'Juice'
+  },
+  {
+    name: 'Homi',
+    category: 'Instant Noodles',
+    subcategory: 'Instant Mami'
+  },
+  {
+    name: 'Lucky Me Mami Beef',
+    category: 'Instant Noodles',
+    subcategory: 'Instant Mami'
+  },
+  {
+    name: 'Lucky Me Mami Chicken',
+    category: 'Instant Noodles',
+    subcategory: 'Instant Mami'
+  },
+  {
+    name: 'Lucky Me Mami Spicy Beef',
+    category: 'Instant Noodles',
+    subcategory: 'Instant Mami'
+  },
+  {
+    name: 'Lucky Me Canton Chilimansi',
     category: 'Instant Noodles',
     subcategory: 'Pancit Canton'
   },
-
+  {
+    name: 'Lucky Me Canton Extra Hot',
+    category: 'Instant Noodles',
+    subcategory: 'Pancit Canton'
+  },
+  {
+    name: 'Lucky Me Canton Kalamansi',
+    category: 'Instant Noodles',
+    subcategory: 'Pancit Canton'
+  },
+  {
+    name: 'Lucky Me Canton Original',
+    category: 'Instant Noodles',
+    subcategory: 'Pancit Canton'
+  },
+  {
+    name: 'Lucky Me Canton Sweet & Spicy',
+    category: 'Instant Noodles',
+    subcategory: 'Pancit Canton'
+  },
+  {
+    name: 'Payless Kalamansi',
+    category: 'Instant Noodles',
+    subcategory: 'Pancit Canton'
+  },
+  {
+    name: 'Payless Original',
+    category: 'Instant Noodles',
+    subcategory: 'Pancit Canton'
+  },
+  {
+    name: 'Nissin Cup Beef',
+    category: 'Instant Noodles',
+    subcategory: 'Cup Noodles'
+  },
+  {
+    name: 'Nissin Cup Seafood',
+    category: 'Instant Noodles',
+    subcategory: 'Cup Noodles'
+  },
+  {
+    name: '555 Sardines Hot & Spicy',
+    category: 'Canned Goods',
+    subcategory: 'Sardines'
+  },
   {
     name: '555 Sardines',
     category: 'Canned Goods',
     subcategory: 'Sardines'
   },
-
   {
-    name: 'Knorr Seasoning',
+    name: 'Ligo Sardines Hot & Spicy',
+    category: 'Canned Goods',
+    subcategory: 'Sardines'
+  },
+  {
+    name: 'Ligo Sardines',
+    category: 'Canned Goods',
+    subcategory: 'Sardines'
+  },
+  {
+    name: 'Mega Sardines Hot & Spicy',
+    category: 'Canned Goods',
+    subcategory: 'Sardines'
+  },
+  {
+    name: 'Mega Sardines',
+    category: 'Canned Goods',
+    subcategory: 'Sardines'
+  },
+  {
+    name: '555 Tuna Afritada',
+    category: 'Canned Goods',
+    subcategory: 'Tuna'
+  },
+  {
+    name: '555 Tuna Caldereta',
+    category: 'Canned Goods',
+    subcategory: 'Tuna'
+  },
+  {
+    name: 'Century Tuna',
+    category: 'Canned Goods',
+    subcategory: 'Tuna'
+  },
+  {
+    name: 'Century Tuna Hot & Spicy',
+    category: 'Canned Goods',
+    subcategory: 'Tuna'
+  },
+  {
+    name: 'Argentina Corned Beef',
+    category: 'Canned Goods',
+    subcategory: 'Corned Beef'
+  },
+  {
+    name: 'CDO Karne Norte',
+    category: 'Canned Goods',
+    subcategory: 'Corned Beef'
+  },
+  {
+    name: 'Purefoods Corned Beef',
+    category: 'Canned Goods',
+    subcategory: 'Corned Beef'
+  },
+  {
+    name: 'Argentina Beef Loaf',
+    category: 'Canned Goods',
+    subcategory: 'Meat Loaf'
+  },
+  {
+    name: 'CDO Meat Loaf',
+    category: 'Canned Goods',
+    subcategory: 'Meat Loaf'
+  },
+  {
+    name: 'Hunts Pork & Beans',
+    category: 'Canned Goods',
+    subcategory: 'Other Canned Goods'
+  },
+  {
+    name: 'Brown Sugar',
+    category: 'Cooking Essentials',
+    subcategory: 'Repacked'
+  },
+  {
+    name: 'Cooking Oil',
+    category: 'Cooking Essentials',
+    subcategory: 'Repacked'
+  },
+  {
+    name: 'Iodized Salt',
+    category: 'Cooking Essentials',
+    subcategory: 'Repacked'
+  },
+  {
+    name: 'White Sugar',
+    category: 'Cooking Essentials',
+    subcategory: 'Repacked'
+  },
+  {
+    name: 'Ajinomoto',
     category: 'Cooking Essentials',
     subcategory: 'Seasonings'
   },
-
   {
-    name: 'Surf Detergent Powder',
+    name: 'Knorr Cube Beef',
+    category: 'Cooking Essentials',
+    subcategory: 'Seasonings'
+  },
+  {
+    name: 'Knorr Cube Chicken',
+    category: 'Cooking Essentials',
+    subcategory: 'Seasonings'
+  },
+  {
+    name: 'Knorr Cube Pork',
+    category: 'Cooking Essentials',
+    subcategory: 'Seasonings'
+  },
+  {
+    name: 'Magic Sarap',
+    category: 'Cooking Essentials',
+    subcategory: 'Seasonings'
+  },
+  {
+    name: 'Oyster Sauce',
+    category: 'Cooking Essentials',
+    subcategory: 'Seasonings'
+  },
+  {
+    name: 'Sinigang Mix',
+    category: 'Cooking Essentials',
+    subcategory: 'Seasonings'
+  },
+  {
+    name: 'Fish Sauce',
+    category: 'Cooking Essentials',
+    subcategory: 'Condiments'
+  },
+  {
+    name: 'Soy Sauce',
+    category: 'Cooking Essentials',
+    subcategory: 'Condiments'
+  },
+  {
+    name: 'Vinegar',
+    category: 'Cooking Essentials',
+    subcategory: 'Condiments'
+  },
+  {
+    name: 'Egg',
+    category: 'Cooking Essentials',
+    subcategory: 'Other Essentials'
+  },
+  {
+    name: 'Ariel Powder',
     category: 'Household Cleaning',
     subcategory: 'Detergent Powder'
   },
-
   {
-    name: 'Safeguard Bath Soap',
+    name: 'Breeze Powder',
+    category: 'Household Cleaning',
+    subcategory: 'Detergent Powder'
+  },
+  {
+    name: 'Champion Powder',
+    category: 'Household Cleaning',
+    subcategory: 'Detergent Powder'
+  },
+  {
+    name: 'Surf Powder',
+    category: 'Household Cleaning',
+    subcategory: 'Detergent Powder'
+  },
+  {
+    name: 'Tide Powder',
+    category: 'Household Cleaning',
+    subcategory: 'Detergent Powder'
+  },
+  {
+    name: 'Perla Bar',
+    category: 'Household Cleaning',
+    subcategory: 'Detergent Bar'
+  },
+  {
+    name: 'Downy Fabcon',
+    category: 'Household Cleaning',
+    subcategory: 'Fabric Conditioner'
+  },
+  {
+    name: 'Surf Fabcon',
+    category: 'Household Cleaning',
+    subcategory: 'Fabric Conditioner'
+  },
+  {
+    name: 'Joy',
+    category: 'Household Cleaning',
+    subcategory: 'Dishwashing'
+  },
+  {
+    name: 'Smart',
+    category: 'Household Cleaning',
+    subcategory: 'Dishwashing'
+  },
+  {
+    name: 'Zonrox',
+    category: 'Household Cleaning',
+    subcategory: 'Other Cleaning Items'
+  },
+  {
+    name: 'Cream Silk',
+    category: 'Personal Care',
+    subcategory: 'Shampoo'
+  },
+  {
+    name: 'Head & Shoulders',
+    category: 'Personal Care',
+    subcategory: 'Shampoo'
+  },
+  {
+    name: 'Palmolive',
+    category: 'Personal Care',
+    subcategory: 'Shampoo'
+  },
+  {
+    name: 'Sunsilk',
+    category: 'Personal Care',
+    subcategory: 'Shampoo'
+  },
+  {
+    name: 'Colgate',
+    category: 'Personal Care',
+    subcategory: 'Toothpaste'
+  },
+  {
+    name: 'Hapee',
+    category: 'Personal Care',
+    subcategory: 'Toothpaste'
+  },
+  {
+    name: 'Toothbrush',
+    category: 'Personal Care',
+    subcategory: 'Toothpaste'
+  },
+  {
+    name: 'Bioderm',
     category: 'Personal Care',
     subcategory: 'Bath Soap'
   },
-
   {
-    name: 'Marlboro Red Cigarettes',
+    name: 'Safeguard',
+    category: 'Personal Care',
+    subcategory: 'Bath Soap'
+  },
+  {
+    name: 'Casino',
+    category: 'Personal Care',
+    subcategory: 'Personal Hygiene'
+  },
+  {
+    name: 'EQ Diaper Large',
+    category: 'Personal Care',
+    subcategory: 'Personal Hygiene'
+  },
+  {
+    name: 'EQ Diaper Medium',
+    category: 'Personal Care',
+    subcategory: 'Personal Hygiene'
+  },
+  {
+    name: 'Cotton Buds',
+    category: 'Personal Care',
+    subcategory: 'Personal Hygiene'
+  },
+  {
+    name: 'Modess',
+    category: 'Personal Care',
+    subcategory: 'Personal Hygiene'
+  },
+  {
+    name: 'Tissue',
+    category: 'Personal Care',
+    subcategory: 'Personal Hygiene'
+  },
+  {
+    name: 'Whisper',
+    category: 'Personal Care',
+    subcategory: 'Personal Hygiene'
+  },
+  {
+    name: 'Gillette',
+    category: 'Personal Care',
+    subcategory: 'Other Care Items'
+  },
+  {
+    name: 'Fortune',
     category: 'Miscellaneous',
     subcategory: 'Cigarettes'
   },
-
   {
-    name: 'Nescafe 3n1 Creamy White',
-    category: 'Coffee & Milk',
-    subcategory: 'Milk'
+    name: 'Marlboro Gold',
+    category: 'Miscellaneous',
+    subcategory: 'Cigarettes'
+  },
+  {
+    name: 'Marlboro Red',
+    category: 'Miscellaneous',
+    subcategory: 'Cigarettes'
+  },
+  {
+    name: 'Mighty',
+    category: 'Miscellaneous',
+    subcategory: 'Cigarettes'
+  },
+  {
+    name: 'Philip Morris',
+    category: 'Miscellaneous',
+    subcategory: 'Cigarettes'
+  },
+  {
+    name: 'Winston',
+    category: 'Miscellaneous',
+    subcategory: 'Cigarettes'
+  },
+  {
+    name: 'Baygon Katol',
+    category: 'Miscellaneous',
+    subcategory: 'Other Items'
+  },
+  {
+    name: 'Candle',
+    category: 'Miscellaneous',
+    subcategory: 'Other Items'
+  },
+  {
+    name: 'Matchbox',
+    category: 'Miscellaneous',
+    subcategory: 'Other Items'
   }
 ]
