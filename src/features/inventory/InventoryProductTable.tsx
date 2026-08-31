a11ee38c088df68409d00de2c44619d63bdf8a22
kentvillamora-dev <kentvillamora.dev@gmail.com>
2026-08-31 17:42:49 +0800

    Added Product Category buttons

--- src/features/inventory/InventoryProductTable.tsx
+++ src/features/inventory/InventoryProductTable.tsx
@@ -1,42 +1,70 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import { productGroups } from './productTypes'
 import { defaultProducts } from './defaultProducts'
 
 function InventoryProductTable() {
+  const [selectedCategory, setSelectedCategory] =
+    useState('All Categories')
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th scope="col">Products</th>
-          <th scope="col">Stock</th>
-          <th scope="col">Unit Cost</th>
-          <th scope="col">SRP</th>
-          <th scope="col">Sell Price</th>
-        </tr>
-      </thead>
+    <>
+      <div>
+        <button
+          onClick={() => setSelectedCategory('All Categories')}
+        >
+          All Categories
+        </button>
 
-      <tbody>
         {Object.keys(productGroups).map((category) => (
-          <Fragment key={category}>
-            <tr>
-              <td colSpan={5}>{category}</td>
-            </tr>
-
-            {defaultProducts
-              .filter((product) => product.category === category)
-              .map((product) => (
-                <tr key={product.name}>
-                  <td>{product.name}</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                  <td>0</td>
-                </tr>
-              ))}
-          </Fragment>
+          <button
+            key={category}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
         ))}
-      </tbody>
-    </table>
+      </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Products</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Unit Cost</th>
+            <th scope="col">SRP</th>
+            <th scope="col">Sell Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {Object.keys(productGroups)
+            .filter(
+              (category) =>
+                selectedCategory === 'All Categories' ||
+                category === selectedCategory
+            )
+            .map((category) => (
+              <Fragment key={category}>
+                <tr>
+                  <td colSpan={5}>{category}</td>
+                </tr>
+
+                {defaultProducts
+                  .filter((product) => product.category === category)
+                  .map((product) => (
+                    <tr key={product.name}>
+                      <td>{product.name}</td>
+                      <td>0</td>
+                      <td>0</td>
+                      <td>0</td>
+                      <td>0</td>
+                    </tr>
+                  ))}
+              </Fragment>
+            ))}
+        </tbody>
+      </table>
+    </>
   )
 }
 
